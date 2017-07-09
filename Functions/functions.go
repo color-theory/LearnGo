3bacb61b3050d67e98d24be23b218d509ed3ad10
John Stringer <jgstringer@gmail.com>
2017-07-09 16:56:15 -0500

    LG-005 Variadic functions

--- Functions/functions.go
+++ Functions/functions.go
@@ -2,6 +2,7 @@
  * Multiple return values
  * Use it like any other type: Similar to functions in js
  * Function literals: When declaring a function inside another function, it remembers context.
+ * Variadic functions: multiple variable parameters in func
  */
 package main
 
@@ -15,13 +16,15 @@ type Salutation struct {
 }
 
 func Greet(salutation Salutation) {
-	first, alternate := CreateMessage(salutation.name, salutation.greeting) //first takes first return from CreateMessage, while alternate gets second returned string.
+	first, alternate := CreateMessage(salutation.name, salutation.greeting, "yo!", "sup!") 	//first takes first return from CreateMessage,
+																					// while alternate gets second returned string. Passing in a 3rd parameter as second greeting
 	fmt.Println(first)
 	fmt.Println(alternate)
 }
 
-func CreateMessage(name, greeting string) (message string, alternate string) { //CreateMessage takes name and greeting as strings and returns two named strings
-	message = greeting + " " + name
+func CreateMessage(name string, greeting ...string) (message string, alternate string) { //CreateMessage takes name and greeting as strings and returns two named strings
+
+	message = greeting[len(greeting)-1] + " " + name //using last greeting passed "sup!"
 	alternate = "Hey! " + name
 	return //returns both message and alternate
 }
