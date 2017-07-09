43d2ed340190967e92e967dbc826341a6deca05c
John Stringer <jgstringer@gmail.com>
2017-07-09 17:33:48 -0500

    LG-009 Closures

--- Functions/functions.go
+++ Functions/functions.go
@@ -3,6 +3,8 @@
  * Use it like any other type: Similar to functions in js
  * Function literals: When declaring a function inside another function, it remembers context.
  * Variadic functions: multiple variable parameters in func
+ * http://golang.org/doc/codewalk/functions/
+ * http://golang.org/ref/spec#Function_Types
  */
 package main
 
@@ -39,8 +41,14 @@ func PrintLine(s string) {
 	fmt.Println(s)
 }
 
+func CreatePrintFunction(custom string) Printer {
+	return func(s string) { //this s will be passed in somewhere else.
+		fmt.Println(s + custom) //uses context from when it was created and imprinting it onto the new function. In this case "!!closure!"
+	}
+}
 func main() {
 	var s = Salutation{"Bob", "Hello"}
-	Greet(s, Print)     //calls Greet with s, and Print func
-	Greet(s, PrintLine) //calls Greet with s, and PrintLine func
+	Greet(s, Print)                             //calls Greet with s, and Print func
+	Greet(s, PrintLine)                         //calls Greet with s, and PrintLine func
+	Greet(s, CreatePrintFunction("!!closure!")) //calls Greet with s and a function that is of type Printer that was created by CreatePrintFunction
 }
