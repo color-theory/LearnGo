ed730c0f7f0bdb2daa88e5870e14094e1ca72433
John Stringer <jgstringer@gmail.com>
2017-07-11 19:41:23 -0500

    LG-39 select statements with channels.

--- 09Concurrency/concurrency.go
+++ 09Concurrency/concurrency.go
@@ -5,6 +5,12 @@
  * Do not communicate by sharing memory; instead, share memory by communicating.
  * Go Routines
  * Lightweight thread. Managed by go runtime. Uses Keyword "go"
+ * channels can be buffered, which allows them to keep pushing messages until the buffer is full, then it is blocked
+ * for range can be used to keep pulling from a channel, but if it is never closed, it will be a deadlock
+ * Select is like a switch, but on communications.
+ * it will execute a case that is "ready"
+ * if more than one is ready, then execute random
+ * if none are ready, block unless default is defined.
  */
 package main
 
@@ -38,10 +44,26 @@ func main() {
 	fmt.Fprintf(&salutations[0], "The count is %d", 10)
 
 	c := make(chan greeting.Salutation) // creating a channel that returns Salutation items
-
+	c2 := make(chan greeting.Salutation) // creating a second channel that returns Salutation items
 	go salutations.ChannelGreeter(c) // start the goroutine
+	go salutations.ChannelGreeter(c2) // start the second goroutine
 
-	for s := range c { // this will loop and pull items from the channel as long as the channel hasn't closed.
-		fmt.Println(s.Name)
+	for{
+		select{
+			case s, ok := <- c:
+				if ok{
+					fmt.Println(s, ": 1")
+				}else{
+					return
+				}
+			case s, ok := <- c2:
+				if ok{
+					fmt.Println(s, ": 2")
+				}else{
+					return
+				}
+			default:
+				fmt.Println("waiting...")
+		}
 	}
 }
