8ff3d086349be58f56375f294791d24fe1e074bb
John Stringer <jgstringer@gmail.com>
2017-07-13 19:20:13 -0500

    LG-45 Using text/template to execute templating

--- WebApplication/main/main.go
+++ WebApplication/main/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"net/http"
+	"text/template"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
+		w.Header().Add("Content Type", "text/html")
+
+		pageData := page{
+			"Example Title",
+			"This is my example header",
+		}
+
+		tmpl, err := template.New("test").Parse(doc)
+		if err == nil{
+			tmpl.Execute(w, pageData)
+		}
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
+
+type page struct{
+	Title string
+	Header string
+}
+
+const doc = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>{{.Title}}</title>
+	</head>
+	<body>
+		<h1>{{.Header}}</h1>
+	</body>
+</html>
+`
